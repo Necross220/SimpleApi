4253e4d5b2b912fdcd9fcb13e5d672e8d290ea2e
Obed David Pérez Uri <Necross220@users.noreply.github.com>
2022-04-07 23:19:05 -0400

    Added phones to create example

--- objects_examples_for_postman.js
+++ objects_examples_for_postman.js
@@ -1,18 +1,19 @@
 //READ
-{
+let data = {
         //http://localhost/Contacts/read -> TODOS
         id: 1 // INDIVIDUAL
 }
 
 //CREATE
-{
+let data = {
         name: 'David',
         lastname: 'Uri',
-        email: 'UPDATED@EMAIL.com'
+        email: 'UPDATED@EMAIL.com',
+        phones: ["809000001", "809000002", "809000003"]
 }
 
 //UPDATE
-{
+let data = {
         id: 1,
         name: 'UPDARTED NAME',
         lastname: 'UPDATED LASTNAME',
@@ -20,7 +21,7 @@
 }
 
 //DELETE
-{
+let data = {
         //Is an update changing the status cause we are not suppose to delete stuff from the database
         id: 1
 }
